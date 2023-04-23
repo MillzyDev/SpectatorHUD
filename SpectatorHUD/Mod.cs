bcda87079def82d71bf61a09cf49e59d09ed4df0
Millzy <millzyyt08@gmail.com>
2023-04-23 19:06:32 +0100

    compat check

--- SpectatorHUD/Mod.cs
+++ SpectatorHUD/Mod.cs
@@ -1,6 +1,7 @@
 ﻿using Boneject;
 using MelonLoader;
 using SpectatorHUD.Modules;
+using UnityEngine;
 
 namespace SpectatorHUD;
 
@@ -8,6 +9,12 @@ public class Mod : MelonMod
 {
     public override void OnInitializeMelon()
     {
+        if (Application.platform != RuntimePlatform.WindowsPlayer)
+        {
+            MelonLogger.Msg("You are trying to use this on an unsupported version. Will not function.");
+            return;
+        }
+        
         MelonLogger.Msg("Registering modules...");
         var bonejector = Bonejector.Instance;
         bonejector.InstallModule<SHGameModule>(InstallLocation.Game);
