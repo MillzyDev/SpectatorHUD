84f80201a4b6453d6485950143458d703f9d6aa8
Millzy <millzyyt08@gmail.com>
2023-05-04 21:03:55 +0100

    basic stuff

---
+++ SpectatorHUD/SpectatorHUD/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using SpectatorHUD;
+using BuildInfo = SpectatorHUD.BuildInfo;
+
+[assembly: AssemblyTitle(BuildInfo.name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(BuildInfo.company)]
+[assembly: AssemblyProduct(BuildInfo.name)]
+[assembly: AssemblyCopyright("""Copyright (c) 2023 Frederick ("Millzy") Mills""")]
+[assembly: AssemblyTrademark(null)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion(BuildInfo.version)]
+[assembly: AssemblyFileVersion(BuildInfo.version)]
+
+[assembly: MelonInfo(typeof(Mod), BuildInfo.name, BuildInfo.semanticVersion, BuildInfo.author, BuildInfo.downloadLink)]
+
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
+[assembly: MelonID(BuildInfo.id)]
+
+[assembly: VerifyLoaderVersion("0.5.7")]
+
+[assembly: HarmonyDontPatchAll]
