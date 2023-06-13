0fc307b3c1b62015176bc7ec2c031c72ef90b0cd
Millzy <millzyyt08@gmail.com>
2023-06-13 22:38:39 +0100

    inject asset container

--- SpectatorHUD/Managers/HUDManager.cs
+++ SpectatorHUD/Managers/HUDManager.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using Ninject;
+﻿using Ninject;
 using UnityEngine;
 
 namespace SpectatorHUD.Managers;
 
 public class HUDManager : MonoBehaviour
 {
+    private HUDAssetContainer _hudAssetContainer = null!;
     private HUDValueManager _hudValueManager = null!;
-    
+
     [Inject]
-    public void Inject(HUDValueManager hudValueManager)
+    public void Inject(HUDAssetContainer hudAssetContainer, HUDValueManager hudValueManager)
     {
+        _hudAssetContainer = hudAssetContainer;
         _hudValueManager = hudValueManager;
     }
 
