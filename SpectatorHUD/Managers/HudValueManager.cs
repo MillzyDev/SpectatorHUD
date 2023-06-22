759dc7e1ee8a73b583fbdbcee15fab32572be9d1
Millzy <millzyyt08@gmail.com>
2023-06-22 10:28:51 +0100

    working value management

--- SpectatorHUD/Managers/HudValueManager.cs
+++ SpectatorHUD/Managers/HudValueManager.cs
@@ -1,21 +1,47 @@
-﻿using Ninject;
+﻿using System;
+using MelonLoader;
+using Ninject;
 using SLZ.Rig;
+using UnityEngine;
 
 namespace SpectatorHUD.Managers
 {
-    public class HudValueManager
+    [RegisterTypeInIl2Cpp]
+    public class HudValueManager : MonoBehaviour
     {
-        private readonly RigManager _rigManager;
+        private RigManager _rigManager = null!;
+        private float _lastHealth = -1f;
 
-        [Inject]
-        public HudValueManager(RigManager rigManager)
+        public event Action<float>? HealthChanged;
+
+        public HudValueManager(IntPtr ptr) : base(ptr)
         {
-            _rigManager = rigManager;
         }
 
-        public float Health
+        private float Health
         {
             get => _rigManager.health.curr_Health;
+        }
+
+        [Inject]
+        public void Inject(RigManager rigManager)
+        {
+            _rigManager = rigManager;
+            
+#if DEBUG
+            HealthChanged += health => MelonLogger.Msg($"Health changed to: {health}");
+#endif
+        }
+
+        private void LateUpdate()
+        {
+            float currentHealth = Health;
+            // ReSharper disable once CompareOfFloatsByEqualityOperator
+            if (!currentHealth.Equals(_lastHealth))
+            {
+                HealthChanged?.Invoke(Health);
+                _lastHealth = currentHealth;
+            }
         }
     }
 }
