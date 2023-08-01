92aab3f74278215f916d6a9f3d806456ccfe1bb5
Millzy <millzyyt08@gmail.com>
2023-08-01 18:36:09 +0100

    bare hud component

---
+++ SpectatorHUD/Hud.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace SpectatorHUD
+{
+    public class Hud : MonoBehaviour
+    {
+        public HudConfigSO hudConfig = null!;
+        
+        public Hud(IntPtr ptr) : base(ptr)
+        {
+        }
+    }
+}
