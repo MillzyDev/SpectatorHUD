405335f2c4537640206d2b3a2533fca7f76dfc28
Millzy <millzyyt08@gmail.com>
2023-08-01 18:36:00 +0100

    SOs

---
+++ SpectatorHUD/HudConfigSO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace SpectatorHUD
+{
+    // ReSharper disable once InconsistentNaming
+    public class HudConfigSO : ScriptableObject
+    {
+        public CombinedAmmoCounterConfig combinedAmmoCounterConfiguration;
+        public HealthCounterConfig healthCounterDisplayMode;
+        
+        public HudConfigSO(IntPtr ptr)
+        {
+        }
+        
+        public enum CombinedAmmoCounterConfig : ushort
+        {
+            CombineAlways = 1,
+            CombineIfOfSameType = 2,
+        }
+        
+        public enum HealthCounterConfig
+        {
+            Value,
+            RawValue,
+            Percentage,
+            GlobalPercentage
+        }
+    }
+}
---
+++ SpectatorHUD/HudManifestSO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace SpectatorHUD
+{
+    // ReSharper disable once InconsistentNaming
+    public class HudManifestSO : ScriptableObject
+    {
+        public string hudName = "Cool hud";
+        public string author = "Me";
+        public ushort version = 1;
+        public string description = "A cool hud.";
+        
+        public HudManifestSO(IntPtr ptr) : base(ptr)
+        {
+        }
+    }
+}
