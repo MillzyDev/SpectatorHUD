a52a590f72264e9e113e0e59f735974e55c8e0bb
Millzy <millzyyt08@gmail.com>
2023-08-12 17:15:43 +0100

    watch health and ammo

---
+++ SpectatorHUD/HudValueWatcher.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Linq;
+using BoneLib;
+using MelonLoader;
+using SLZ.Interaction;
+using SLZ.Props.Weapons;
+using SLZ.Rig;
+using UnhollowerBaseLib.Attributes;
+using UnityEngine;
+
+namespace SpectatorHUD
+{
+    [RegisterTypeInIl2Cpp]
+    public class HudValueWatcher : MonoBehaviour
+    {
+        private static readonly Lazy<HudValueWatcher> s_lazy = new(Construct);
+
+        private RigManager _rigManager = null!;
+        private Hand _leftHand = null!;
+        private Hand _rightHand = null!;
+        
+        private Action? _onValueCheck;
+        public Action<float, float>? OnHealth;
+        public Action<int, int, bool>? OnAmmo;
+
+        private Health _health = null!;
+
+        private Gun? _leftGun;
+        private Gun? _rightGun;
+
+        private float _lastHealth;
+        private float _lastMaxHealth;
+        private int _lastLeftAmmo;
+        private int _lastRightAmmo;
+        private bool _lastSameGun;
+
+        public HudValueWatcher(IntPtr ptr) : base(ptr)
+        {
+        }
+
+        public static HudValueWatcher Instance
+        {
+            get => s_lazy.Value;
+        }
+
+        [HideFromIl2Cpp]
+        private static HudValueWatcher Construct()
+        {
+            HudValueWatcher? any = Resources.FindObjectsOfTypeAll<HudValueWatcher>().FirstOrDefault();
+            if (any) return any!; // should realistically never happen.
+
+            var gameObject = new GameObject(typeof(HudValueWatcher).FullName);
+            var hudValueWatcher = gameObject.AddComponent<HudValueWatcher>();
+            DontDestroyOnLoad(hudValueWatcher);
+
+            return hudValueWatcher;
+        }
+
+        public void BeginWatch(RigManager rigManager)
+        {
+            _rigManager = rigManager;
+            
+            _leftHand = rigManager.physicsRig.leftHand;
+            _leftHand.onRecieverAttached += (Action<HandReciever>)LeftHandAttach;
+            _leftHand.onRecieverDetached += (Action<HandReciever>)LeftHandDetach;
+            
+            _rightHand = rigManager.physicsRig.rightHand;
+            _rightHand.onRecieverAttached += (Action<HandReciever>)RightHandAttach;
+            _rightHand.onRecieverDetached += (Action<HandReciever>)RightHandDetach;
+            
+            _health = rigManager.health;
+            
+            _onValueCheck += CheckHealth;
+            _onValueCheck += CheckAmmo;
+        }
+
+        public void EndWatch()
+        {
+            _leftHand.onRecieverAttached -= (Action<HandReciever>)LeftHandAttach;
+            _leftHand.onRecieverDetached -= (Action<HandReciever>)LeftHandDetach;
+            _rightHand.onRecieverAttached -= (Action<HandReciever>)RightHandAttach;
+            _rightHand.onRecieverDetached -= (Action<HandReciever>)RightHandDetach;
+            
+            _rigManager = null!;
+            _leftHand = null!;
+            _rightHand = null!;
+            _health = null!;
+            
+            _onValueCheck = null;
+        }
+
+        private void Update()
+        {
+            _onValueCheck?.Invoke();
+        }
+
+        private void CheckHealth()
+        {
+            float health = _health.curr_Health;
+            float maxHealth = _health.max_Health;
+            
+            if (!health.Equals(_lastHealth) || !maxHealth.Equals(_lastMaxHealth))
+                OnHealth?.Invoke(health, maxHealth);
+            
+            _lastHealth = health;
+            _lastMaxHealth = maxHealth;
+        }
+
+        private void CheckAmmo()
+        {
+            int leftAmmo = _leftGun ? _leftGun!.AmmoCount() : 0;
+            int rightAmmo = _rightGun ? _rightGun!.AmmoCount() : 0;
+            bool sameGun = _leftGun == _rightGun;
+
+            if (leftAmmo != _lastLeftAmmo || rightAmmo != _lastRightAmmo || sameGun != _lastSameGun)
+                OnAmmo?.Invoke(leftAmmo, rightAmmo, sameGun);
+            
+            _lastLeftAmmo = leftAmmo;
+            _lastRightAmmo = rightAmmo;
+            _lastSameGun = sameGun;
+        }
+
+        private void LeftHandAttach(HandReciever _)
+        {
+            _leftGun = Player.GetGunInHand(_leftHand) ?? null;
+        }
+
+        private void LeftHandDetach(HandReciever _)
+        {
+            _leftGun = null;
+        }
+
+        private void RightHandAttach(HandReciever _)
+        {
+            _rightGun = Player.GetGunInHand(_rightHand) ?? null;
+        }
+
+        private void RightHandDetach(HandReciever _)
+        {
+            _rightGun = null;
+        }
+    }
+}
