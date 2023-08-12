0adee842200391d3c0cc12dcc9ac15e9c6de77d8
Millzy <millzyyt08@gmail.com>
2023-08-12 14:44:14 +0100

    hud loader singleton

---
+++ SpectatorHUD/HudLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using MelonLoader;
+using UnityEngine;
+
+namespace SpectatorHUD
+{
+    internal class HudLoader
+    {
+        private static readonly Lazy<HudLoader> s_lazy = new(
+            () => new HudLoader()
+            );
+
+        private static string? s_hudsDirectory;
+        
+        private GameObject _hudRoot = null!;
+
+        private HudLoader()
+        {
+        }
+
+        public static HudLoader Instance
+        {
+            get => s_lazy.Value;
+        }
+
+        public static string HudsDirectory
+        {
+            get => s_hudsDirectory ??= Path.Combine(MelonUtils.UserDataDirectory, "SpectatorHUD", "HUDs");
+        }
+    }
+}
