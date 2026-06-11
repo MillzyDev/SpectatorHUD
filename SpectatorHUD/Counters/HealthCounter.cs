1a7e4505e4309ab804470ff74dbd2041f8fc7c22
millz <f.w.b.mills@gmail.com>
2026-06-11 10:58:46 +0100

    License

--- SpectatorHUD/Counters/HealthCounter.cs
+++ SpectatorHUD/Counters/HealthCounter.cs
@@ -1,4 +1,22 @@
-﻿using Il2CppSLZ.Marrow;
+﻿/*
+ *      SpectatorHUD
+ *      Copyright (C) 2026  Millzy
+ *
+ *      This program is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
+using Il2CppSLZ.Marrow;
 using Il2CppTMPro;
 using Il2CppUltEvents;
 using UnityEngine;
