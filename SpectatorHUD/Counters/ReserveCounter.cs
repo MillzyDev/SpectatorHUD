0676cb164eb303016f5c3c1640613b8058f35d58
millz <f.w.b.mills@gmail.com>
2026-06-18 19:16:43 +0100

    reserve counter

---
+++ SpectatorHUD/Counters/ReserveCounter.cs
@@ -0,0 +1,63 @@
+﻿/*
+ *      SpectatorHUD
+ *      Copyright (C) 2026  Millzy
+ *
+ *      This program is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
+using Il2CppSLZ.Marrow;
+using Il2CppSLZ.Marrow.Data;
+using Il2CppTMPro;
+using Il2CppUltEvents;
+using UnityEngine;
+
+namespace SpectatorHUD.Counters
+{
+    public class ReserveCounter : MonoBehaviour
+    {
+        [NonSerialized] public RigManager? rigManager;
+        [NonSerialized] public Gun? heldGun;
+        [NonSerialized] public UltEvent<float> onChange;
+        
+        private TMP_Text? _counterText;
+        private int _lastObserved = 0;
+        
+        public ReserveCounter(IntPtr ptr) : base(ptr)
+        {
+        }
+
+        public void Start()
+        {
+            this._counterText = this.GetComponent<TMP_Text>();
+        }
+
+        public void Update()
+        {
+            CartridgeData? cartridgeData = this.heldGun?.defaultCartridge;
+            int cartridgeCount = this.heldGun?._ammoInventory.GetCartridgeCount(cartridgeData) ?? 0;
+
+            if (cartridgeCount == this._lastObserved)
+            {
+                return;
+            }
+            
+            int reserve = cartridgeCount - (this.heldGun?.AmmoCount() ?? 0);
+            
+            Logger.Debug("Reserve changed: {0} -> {1}", this._lastObserved, reserve);
+            this._lastObserved = reserve;
+            this._counterText?.SetText(reserve.ToString());
+            // this.onChange?.Invoke(); TODO
+        }
+    }
+}
--- SpectatorHUD/HudManagerV1.cs
+++ SpectatorHUD/HudManagerV1.cs
@@ -54,6 +54,8 @@ namespace SpectatorHUD
         private HealthCounter? _healthCounter;
         private AmmoCounter? _leftAmmoCounter;
         private AmmoCounter? _rightAmmoCounter;
+        private ReserveCounter? _leftReserveCounter;
+        private ReserveCounter? _rightReserveCounter;
 
         private Il2CppSystem.Action<HandReciever>? _onLeftHandAttached;
         private Il2CppSystem.Action<HandReciever>? _onRightHandAttached;
@@ -78,31 +80,48 @@ namespace SpectatorHUD
             this._leftHand = this._physicsRig?.leftHand;
             this._rightHand = this._physicsRig?.rightHand;
             
+            Gun? leftGun = this.GetGun(this._physicsRig?.leftHand);
+            Gun? rightGun = this.GetGun(this._physicsRig?.rightHand);
+            
             if (this.healthCounter != null)
             {
                 this._healthCounter = this.healthCounter.gameObject.AddComponent<HealthCounter>();
                 this._healthCounter.rigManager = this.rigManager;
                 this._healthCounter.onChange = this.healthCounterChanged;
             }
+            
+            this._leftHand?.onRecieverAttached += this._onLeftHandAttached;
+            this._leftHand?.onRecieverDetached += this._onLeftHandDetached;
+            this._rightHand?.onRecieverAttached += this._onRightHandAttached;
+            this._rightHand?.onRecieverDetached += this._onRightHandDetached;
 
             if (this.leftHandAmmoCounter != null)
             {
                 this._leftAmmoCounter = this.leftHandAmmoCounter.gameObject.AddComponent<AmmoCounter>();
                 this._leftAmmoCounter.rigManager = this.rigManager;
-                this._leftAmmoCounter.heldGun = this.GetGun(this._physicsRig?.leftHand);
-
-                this._leftHand?.onRecieverAttached += this._onLeftHandAttached;
-                this._leftHand?.onRecieverDetached += this._onLeftHandDetached;
+                this._leftAmmoCounter.heldGun = leftGun;
             }
 
             if (this.rightHandAmmoCounter != null)
             {
                 this._rightAmmoCounter = this.rightHandAmmoCounter.gameObject.AddComponent<AmmoCounter>();
                 this._rightAmmoCounter.rigManager = this.rigManager;
-                this._rightAmmoCounter.heldGun = this.GetGun(this._physicsRig?.leftHand);
+                this._rightAmmoCounter.heldGun = rightGun;
                 
-                this._rightHand?.onRecieverAttached += this._onRightHandAttached;
-                this._rightHand?.onRecieverDetached += this._onRightHandDetached;
+            }
+
+            if (this.leftHandReserveCounter != null)
+            {
+                this._leftReserveCounter = this.leftHandReserveCounter.gameObject.AddComponent<ReserveCounter>();
+                this._leftReserveCounter.rigManager = this.rigManager;
+                this._leftReserveCounter.heldGun = leftGun;
+            }
+
+            if (this._rightReserveCounter != null)
+            {
+                this._rightReserveCounter = this.rightHandReserveCounter.gameObject.AddComponent<ReserveCounter>();
+                this._rightReserveCounter.rigManager = this.rigManager;
+                this._rightReserveCounter.heldGun = rightGun;
             }
         }
         
@@ -119,37 +138,56 @@ namespace SpectatorHUD
                 return null;
             }
 
-            var gun = this.GetComponentInParent<Gun>();
-            return gun == null ? this.GetComponentInChildren<Gun>() : gun;
+            Logger.Debug("Getting gun component");
+            var gun = heldObject.GetComponentInParent<Gun>();
+            return gun;
         }
 
         private void LeftHandAttached(HandReciever _)
         {
             this._leftAmmoCounter?.enabled = true;
-            this._leftAmmoCounter?.heldGun = this.GetGun(this._leftHand);
-            Logger.Debug("Gun {0} attached", this._leftAmmoCounter?.heldGun?.name ?? "");
+            this._leftReserveCounter?.enabled = true;
+            
+            Gun? heldGun = this.GetGun(this._leftHand);
+            this._leftAmmoCounter?.heldGun = heldGun;
+            this._leftReserveCounter?.heldGun = heldGun;
+            
+            Logger.Debug("Gun {0} attached", heldGun?.gameObject.name ?? "NULL");
         }
 
         private void RightHandAttached(HandReciever _)
         {
             this._rightAmmoCounter?.enabled = true;
-            this._rightAmmoCounter?.heldGun = this.GetGun(this._rightHand);
-            Logger.Debug("Gun {0} attached", this._rightAmmoCounter?.heldGun?.name ?? "");
+            this._rightReserveCounter?.enabled = true;
+            
+            Gun? heldGun = this.GetGun(this._rightHand);
+            this._rightAmmoCounter?.heldGun = heldGun;
+            this._rightReserveCounter?.heldGun = heldGun;
+            
+            Logger.Debug("Gun {0} attached", this._rightAmmoCounter?.heldGun?.gameObject.name ?? "");
             
         }
 
         private void LeftHandDetached(HandReciever _)
         {
-            Logger.Debug("Gun {0} detached", this._leftAmmoCounter?.heldGun?.name ?? "");
+            Logger.Debug("Gun {0} detached", this._leftAmmoCounter?.heldGun?.gameObject.name ?? "");
+            
             this._leftAmmoCounter?.heldGun = null;
+            this._leftReserveCounter?.heldGun = null;
+            
             this._leftAmmoCounter?.enabled = false;
+            this._leftReserveCounter?.enabled = false;
         }
 
         private void RightHandDetached(HandReciever _)
         {
-            Logger.Debug("Gun {0} detached", this._rightAmmoCounter?.heldGun?.name ?? "");
+            Logger.Debug("Gun {0} detached", this._rightAmmoCounter?.heldGun?.gameObject.name ?? "");
+            
             this._rightAmmoCounter?.heldGun = null;
+            this._rightReserveCounter?.heldGun = null;
+            
             this._rightAmmoCounter?.enabled = false;
+            this._rightReserveCounter?.enabled = false;
         }
     }
 }
